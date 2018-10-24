a9e0b433673ead21ff62d4fba63f660c88a30f61
Kevin Conway <kconway@atlassian.com>
2018-10-24 09:58:26 -0500

    Add initial content from template

---
+++ pkg/errs/doc.go
@@ -0,0 +1,4 @@
+// Package errs is a container for all domain errors leveraged by the service.
+// Each error here should represent a specific condition that needs to be
+// communicated across interface boundaries.
+package errs
